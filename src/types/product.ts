8a0361d1a5df3046765d94bf256fd7b1a2304822
EYE <EYE@EYEz>
2023-10-09 14:49:11 +0300

    добавит типы

---
+++ src/types/product.ts
@@ -0,0 +1,18 @@
+export type Product = {
+  id: number;
+  name: string;
+  vendorCode: string;
+  type: string;
+  category: string;
+  description: string;
+  level: string;
+  price: number;
+  rating: number;
+  reviewCount: number;
+  previewImg: string;
+  previewImg2x: string;
+  previewImgWebp: string;
+  previewImgWebp2x: string;
+};
+
+export type Products = Product[];
