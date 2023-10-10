fd5340cc9e91d5976291a883241254995d73d94f
EYE <EYE@EYEz>
2023-10-10 20:39:07 +0300

    Добавит слайдер с моками

--- src/components/banner-element/banner-element.tsx
+++ src/components/banner-element/banner-element.tsx
@@ -1,31 +1,61 @@
+import { Autoplay, Pagination } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+import './swiper.css';
+import { promo } from './mock';
+import { Link } from 'react-router-dom';
+import { AppRoutes } from '../../const';
+
 export function BannerElement() {
   return (
-    <div className="banner">
-      <picture>
-        <source
-          type="image/webp"
-          srcSet="img/content/banner-bg.webp, img/content/banner-bg@2x.webp 2x"
-        />
-        <img
-          src="img/content/banner-bg.jpg"
-          srcSet="img/content/banner-bg@2x.jpg 2x"
-          width={1280}
-          height={280}
-          alt="баннер"
-        />
-      </picture>
-      <p className="banner__info">
-        <span className="banner__message">Новинка!</span>
-        <span className="title title--h1">
-          Cannonball&nbsp;Pro&nbsp;MX&nbsp;8i
-        </span>
-        <span className="banner__text">
-          Профессиональная камера от&nbsp;известного производителя
-        </span>
-        <a className="btn" href="#">
-          Подробнее
-        </a>
-      </p>
-    </div>
+    <Swiper
+      slidesPerView={1}
+      spaceBetween={30}
+      loop
+      centeredSlides
+      autoplay={{
+        delay: 3000,
+        disableOnInteraction: true,
+      }}
+      pagination={{
+        clickable: true,
+      }}
+      modules={[Autoplay, Pagination]}
+    >
+      {promo.map((item) => (
+        <SwiperSlide key={item.id}>
+          <div className="banner">
+            <picture>
+              <source
+                type="image/webp"
+                srcSet={`${item.previewImgWebp}, ${item.previewImgWebp2x} 2x`}
+              />
+              <img
+                src={item.previewImg}
+                srcSet={`${item.previewImg2x} 2x`}
+                width={1280}
+                height={280}
+                alt={item.name}
+              />
+            </picture>
+            <p className="banner__info">
+              <span className="banner__message">Новинка!</span>
+              <span className="title title--h1">
+                {item.name}
+              </span>
+              <span className="banner__text">
+                Профессиональная камера от&nbsp;известного производителя
+              </span>
+              <Link className="btn" to={`${AppRoutes.Product}/${item.id}`}>
+                Подробнее
+              </Link>
+            </p>
+          </div>
+        </SwiperSlide>
+      ))}
+    </Swiper>
   );
 }
--- src/const.ts
+++ src/const.ts
@@ -1,3 +1,8 @@
 export const APIRoutes = {
   Products: '/cameras',
-};
+} as const;
+
+export const AppRoutes = {
+  Main: '/',
+  Product: '/product',
+} as const;
