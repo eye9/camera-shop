3716e83f86d6b3310bd25f376d53e11a6961d642
EYE <EYE@EYEz>
2023-10-10 19:18:42 +0300

    Подключит хранилище

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './components/app/app';
+import { Provider } from 'react-redux';
+import { store } from './store/store';
+import { fetchProductsAction } from './store/api-actions';
+
+store.dispatch(fetchProductsAction());
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -8,6 +13,8 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App></App>
+    </Provider>
   </React.StrictMode>
 );
