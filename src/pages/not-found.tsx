d169722e80f7ccab001bd1e154d9e9a7c25e1b01
EYE <EYE@EYEz>
2023-10-11 18:44:44 +0300

    Добавит страницу 404

---
+++ src/pages/not-found.tsx
@@ -0,0 +1,15 @@
+import { Link } from 'react-router-dom';
+import { AppRoutes } from '../const';
+import { Helmet } from 'react-helmet-async';
+
+export function NotFound() {
+  return (
+    <div className="wrapper">
+      <Helmet>
+        <title>Страница не найдена - Фотошоп</title>
+      </Helmet>
+      <h1>404 Not Found</h1>
+      <Link to={AppRoutes.Main}>На главную</Link>
+    </div>
+  );
+}
