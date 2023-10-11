b82f07238e0f9cf184f7e2af83c4c42c15803d6e
EYE <EYE@EYEz>
2023-10-11 17:55:44 +0300

    Добавит навигацию в заголовке

--- src/components/header-element/header-element.tsx
+++ src/components/header-element/header-element.tsx
@@ -1,35 +1,38 @@
+import { Link } from 'react-router-dom';
+import { AppRoutes } from '../../const';
+
 export function HeaderElement() {
   return (
     <header className="header" id="header">
       <div className="container">
-        <a
+        <Link
           className="header__logo"
-          href="index.html"
+          to={AppRoutes.Main}
           aria-label="Переход на главную"
         >
           <svg width={100} height={36} aria-hidden="true">
             <use xlinkHref="#icon-logo" />
           </svg>
-        </a>
+        </Link>
         <nav className="main-nav header__main-nav">
           <ul className="main-nav__list">
             <li className="main-nav__item">
-              <a className="main-nav__link" href="catalog.html">
+              <Link className="main-nav__link" to={AppRoutes.Main}>
                 Каталог
-              </a>
+              </Link>
             </li>
             <li className="main-nav__item">
-              <a className="main-nav__link" href="#">
+              <a className="main-nav__link">
                 Гарантии
               </a>
             </li>
             <li className="main-nav__item">
-              <a className="main-nav__link" href="#">
+              <a className="main-nav__link">
                 Доставка
               </a>
             </li>
             <li className="main-nav__item">
-              <a className="main-nav__link" href="#">
+              <a className="main-nav__link">
                 О компании
               </a>
             </li>
