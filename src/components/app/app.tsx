f09719eb6f190457cce5e6b06843e6d8f9f8b251
EYE <EYE@EYEz>
2023-10-11 17:52:47 +0300

    Добавит компонент скролла

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,13 +1,18 @@
 import { HelmetProvider } from 'react-helmet-async';
 import { MainPage } from '../../pages/main-page';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { AppRoutes } from '../../const';
+import { ProductPage } from '../../pages/product-page';
+import ScrollToTop from '../scroll-top';
 
 export function App() {
   return (
     <HelmetProvider>
       <BrowserRouter>
+        <ScrollToTop />
         <Routes>
-          <Route path={'/'} element={<MainPage />} />
+          <Route path={AppRoutes.Main} element={<MainPage />} />
+          <Route path={`${AppRoutes.Product}/:id`} element={<ProductPage />} />
         </Routes>
       </BrowserRouter>
     </HelmetProvider>
