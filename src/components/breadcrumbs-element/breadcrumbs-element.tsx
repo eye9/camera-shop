f1593a1f0b3bd389b17425171755ef92b6493b94
EYE <EYE@EYEz>
2023-10-11 17:56:12 +0300

    Добавит хлебные крошки

---
+++ src/components/breadcrumbs-element/breadcrumbs-element.tsx
@@ -0,0 +1,48 @@
+import { Link } from 'react-router-dom';
+import { AppRoutes } from '../../const';
+
+type BreadcrumbsElementProps = {
+  productName: string | undefined;
+};
+
+export function BreadcrumbsElement({ productName }: BreadcrumbsElementProps) {
+  return (
+    <div className="breadcrumbs">
+      <div className="container">
+        <ul className="breadcrumbs__list">
+          <li className="breadcrumbs__item">
+            <Link className="breadcrumbs__link" to={AppRoutes.Main}>
+              Главная
+              <svg width={5} height={8} aria-hidden="true">
+                <use xlinkHref="#icon-arrow-mini" />
+              </svg>
+            </Link>
+          </li>
+          {!productName ? (
+            <li className="breadcrumbs__item">
+              <span className="breadcrumbs__link breadcrumbs__link--active">
+                Каталог
+              </span>
+            </li>
+          ) : (
+            <>
+              <li className="breadcrumbs__item">
+                <Link className="breadcrumbs__link" to={AppRoutes.Main}>
+                  Каталог
+                  <svg width={5} height={8} aria-hidden="true">
+                    <use xlinkHref="#icon-arrow-mini" />
+                  </svg>
+                </Link>
+              </li>
+              <li className="breadcrumbs__item">
+                <span className="breadcrumbs__link breadcrumbs__link--active">
+                  {productName}
+                </span>
+              </li>
+            </>
+          )}
+        </ul>
+      </div>
+    </div>
+  );
+}
