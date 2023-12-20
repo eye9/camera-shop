5aa2cbf24d270ea6cf53ebeea2cbd4edb44b9d4e
EYE <EYE@EYEz>
2023-12-20 17:03:23 +0300

    fix bug 8

--- src/components/in-busket-button/in-busket-button.tsx
+++ src/components/in-busket-button/in-busket-button.tsx
@@ -3,7 +3,7 @@ import { AppRoutes } from '../../const';
 
 export function InBusketButton() {
   return (
-    <Link className="btn btn--purple-border" to={`${AppRoutes.Busket}`}>
+    <Link className="btn btn--purple-border product-card__btn product-card__btn--in-cart" to={`${AppRoutes.Busket}`}>
       <svg width={16} height={16} aria-hidden="true">
         <use xlinkHref="#icon-basket" />
       </svg>
